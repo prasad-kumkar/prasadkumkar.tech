422f34ab58eb934f3094d0711859d54cf2311f08
Prasad Kumkar <pk111_@live.com>
2020-11-23 13:40:33 +0530

    Added header with social media links

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header/>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&display=swap" rel="stylesheet">
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  font-family: 'Homemade Apple', 'Courier New', Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #2c3e50;
+  color: white;
+  margin-top: 20px;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+  <h1>prasad kumkar</h1>
+  <div class="social-media">
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/016-discord.png"></a>
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/090-telegram.png"></a>
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/053-medium.png"></a>
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/096-twitter.png"></a>
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/032-github.png"></a>
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/052-linkedin.png"></a>
+    <a class="social-media-links" href=""><img class="social-media-icons" src="../assets/078-slack.png"></a>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.social-media{
+  margin-top: 20px;
+}
+.social-media-icons{
+  height: 40px;
+}
+.social-media-links{
+  margin: 0px 10px 0px 10px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
