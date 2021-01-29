fa6a8b10b7f5808887ef29cf25ecf1c9f7f59f01
Prasad Kumkar <prasadsanjay.kumkar2018@vitstudent.ac.in>
2021-01-29 21:21:35 +0530

    new site

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
 createApp(App).mount('#app')
+new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount("#app");
